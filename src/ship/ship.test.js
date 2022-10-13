d895c8acf882ee9d7870a62c9b053d80de77dfb6
KingCobra29A <brunerk@umich.edu>
2022-10-13 15:59:22 -0400

    refactor

--- src/ship/ship.test.js
+++ src/ship/ship.test.js
@@ -1,7 +1,7 @@
 const ship = require("./ship");
 
 beforeEach(() => {
-  shipInstance = ship(5);
+  shipInstance = ship("carrier");
 });
 
 afterEach(() => {
@@ -13,37 +13,25 @@ test("hit method: test return value", () => {
 });
 
 test("isSunk method: test return value on healthy ship", () => {
-  expect(
-    (() => {
-      shipInstance.hit();
-      return shipInstance.isSunk();
-    })()
-  ).toBe(false);
+  shipInstance.hit();
+  expect(shipInstance.isSunk()).toBe(false);
 });
 
 test("Happy path: ship(5) shot 5 times, check if sunk", () => {
-  expect(
-    (() => {
-      shipInstance.hit();
-      shipInstance.hit();
-      shipInstance.hit();
-      shipInstance.hit();
-      shipInstance.hit();
-      return shipInstance.isSunk();
-    })()
-  ).toBe(true);
+  shipInstance.hit();
+  shipInstance.hit();
+  shipInstance.hit();
+  shipInstance.hit();
+  shipInstance.hit();
+  expect(shipInstance.isSunk()).toBe(true);
 });
 
 test("Unhappy path: ship(5) shot 6 times, check if sunk", () => {
-  expect(
-    (() => {
-      shipInstance.hit();
-      shipInstance.hit();
-      shipInstance.hit();
-      shipInstance.hit();
-      shipInstance.hit();
-      shipInstance.hit();
-      return shipInstance.isSunk();
-    })()
-  ).toBe(true);
+  shipInstance.hit();
+  shipInstance.hit();
+  shipInstance.hit();
+  shipInstance.hit();
+  shipInstance.hit();
+  shipInstance.hit();
+  expect(shipInstance.isSunk()).toBe(true);
 });

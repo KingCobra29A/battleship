cf7de69f43dbbb36bd82a2b4299ea3556e756127
KingCobra29A <brunerk@umich.edu>
2022-10-18 15:21:08 -0400

    Add pubSub module

---
+++ src/utilities/pubSub.js
@@ -0,0 +1,26 @@
+const PubSub = (() => {
+  const subscribers = {};
+
+  function publish(eventName, data) {
+    if (!Array.isArray(subscribers[eventName])) {
+      return;
+    }
+    subscribers[eventName].forEach((callback) => {
+      callback(data);
+    });
+  }
+
+  function subscribe(eventName, callback) {
+    if (!Array.isArray(subscribers[eventName])) {
+      subscribers[eventName] = [];
+    }
+    subscribers[eventName].push(callback);
+  }
+
+  return {
+    publish,
+    subscribe,
+  };
+})();
+
+module.exports = PubSub;

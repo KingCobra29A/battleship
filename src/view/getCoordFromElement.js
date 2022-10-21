e82c7456031efb9690892896b608aa2884f9ad3c
KingCobra29A <brunerk@umich.edu>
2022-10-21 12:10:52 -0400

    Add function

---
+++ src/view/getCoordFromElement.js
@@ -0,0 +1,10 @@
+// Helper function for queryPlacement
+// Input parameter element is expected to be a "grid-square"
+// returns coordinate object which can be used by the player object
+function getCoordFromElement(element) {
+  const row = Number(element.getAttribute("row"));
+  const column = Number(element.getAttribute("column"));
+  return { row, column };
+}
+
+export default getCoordFromElement;

bab0300f4a7b147ec963a6fed496fdeef1ce57b9
KingCobra29A <brunerk@umich.edu>
2022-10-24 20:49:52 -0400

    Publish game-start event. Add temporary debug output

--- src/gameloop/gameloop.js
+++ src/gameloop/gameloop.js
@@ -1,12 +1,13 @@
 import Player from "../player/player";
 import Board from "../gameboard/gameboard";
+import PubSub from "../utilities/pubSub";
 
 const GameLoop = async () => {
   const humanBoard = Board();
   const enemyBoard = Board();
 
   const human = Player("human", humanBoard, enemyBoard);
-  const enemy = Player("computer", humanBoard, enemyBoard);
+  const enemy = Player("computer", enemyBoard, humanBoard);
 
   //  SETUP BLOCK
   //  Ships get placed by each player
@@ -18,25 +19,32 @@ const GameLoop = async () => {
   //  INITIALIZATION BLOCK
   //  Game is now initialized to begin. Gameloop should notify UI
   //    At this point, board receives attacks, not ships
-  const emitGameStartEvent = (() => {})();
+  PubSub.publish("game-start", "");
 
   // TURN BLOCK
   // Check if game is won/lost
   //    if won/lost, goto endgame
   //    else
   //      Next player goes
+  let turnNumber = 0;
   while (true) {
+    console.log(turnNumber);
     await human.takeMove();
     if (enemyBoard.checkForVictory) {
       // Human player won
+      console.log("Human won");
       break;
     }
+    console.log("Spaghetti");
     await enemy.takeMove();
     if (humanBoard.checkForVictory) {
       // Human player won
+      console.log("computer won");
       break;
     }
+    turnNumber += 1;
   }
+  console.log("GameOver");
 
   // ENDGAME BLOCK
   const winner = enemyBoard.checkForVictory ? human : enemy;
